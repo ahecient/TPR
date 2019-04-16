644f37e4f4970d1b90723c12b12ffe15cab99191
ahecient <48700087+ahecient@users.noreply.github.com>
2019-04-16 21:58:50 +0300

    + counter; trouble in NFA, edit BFA

--- TPR2/lab2.R
+++ TPR2/lab2.R
@@ -14,10 +14,12 @@ min_count_containers <- function(file){
 }
 
 NFA <- function(file){
+  count <- 0
   list_containers <- list()
   list_elements <- list()
   
   for(i in 1:length(file)){
+    count <- count + 1
     if((do.call(sum, list_elements)+file[i]) <= 100){
       list_elements <- append(list_elements, file[i]) 
     }
@@ -28,24 +30,26 @@ NFA <- function(file){
       list_elements <- append(list_elements, file[i])
     }
   }
-  save(list_containers, file = "NFA.RData")
+  save(list_containers, list_elements, file = "NFA.RData")
   containers_count <- length(list_containers)
-  return(containers_count)
+  return(c(containers_count, count))
 }
 
 FFA <- function(file){
-
+  count <- 0
   list_containers <- list()
   list_elements <- list()
   
   for(i in 1:length(file)){
     mark <- TRUE
+    count <- count + 1
     if((do.call(sum, list_elements)+file[i]) <= 100){
       list_elements <- append(list_elements, file[i]) 
     }
     else{
       list_containers <-append(list_containers, list(list_elements))
-      for (j in 1:length(list_containers)){
+      for (j in length(list_containers):1){
+        count <- count + 1
         if((do.call(sum, list_containers[[j]])+file[i]) <= 100){
           list_containers[[j]] <- append(list_containers[[j]], file[i])
           mark <- FALSE
@@ -54,6 +58,7 @@ FFA <- function(file){
       }
       list_elements <- NULL
       list_elements <- list()
+      #count <- count + 1
       if (mark){
         list_elements <- append(list_elements, file[i])
       }
@@ -61,31 +66,36 @@ FFA <- function(file){
   }
   save(list_containers, file = "FFA.RData")
   containers_count <- length(list_containers)
-  return(containers_count)
+  return(c(containers_count, count))
 }
 
 WFA <- function(file){
+  count <- 0
   list_containers <- list()
   list_elements <- list()
   
   for(i in 1:length(file)){
     h <- integer()
     mark <- TRUE
+    count <- count +1
     if((do.call(sum, list_elements)+file[i]) <= 100){
       list_elements <- append(list_elements, file[i]) 
     }
     else{
       list_containers <-append(list_containers, list(list_elements))
       for (j in 1:length(list_containers)){
+        count <- count + 1
         h <- append(h, (100 - do.call(sum, list_containers[[j]])))
       }
       max_f <- which(h == max(h), arr.ind = TRUE)
+      count <- count + 1
       if((do.call(sum, list_containers[[max_f[1]]])+file[i]) <= 100){
         list_containers[[max_f[1]]] <- append(list_containers[[max_f[1]]], file[i])
         mark <- FALSE
       }
       list_elements <- NULL
       list_elements <- list()
+      #count <- count +1
       if (mark){
         list_elements <- append(list_elements, file[i])
       }
@@ -93,25 +103,29 @@ WFA <- function(file){
   }
   save(list_containers, file = "WFA.RData")
   containers_count <- length(list_containers)
-  return(containers_count)
+  return(c(containers_count, count))
 }
 
 BFA <- function(file){
+  count <- 0
   list_containers <- list()
   list_elements <- list()
   
   for(i in 1:length(file)){
     h <- integer()
     mark <- TRUE
+    count <- count + 1
     if((do.call(sum, list_elements)+file[i]) <= 100){
       list_elements <- append(list_elements, file[i]) 
     }
     else{
       list_containers <-append(list_containers, list(list_elements))
       for (j in 1:length(list_containers)){
+        count <- count + 1
         h <- append(h, (100 - do.call(sum, list_containers[[j]])))
       }
       if(!is_empty(min(h))){
+        count <- count + 1
         if((do.call(sum, list_containers[[min(h)]])+file[i]) <= 100){
           list_containers[[min(h)]] <- append(list_containers[[min(h)]], file[i])
           mark <- FALSE
@@ -126,10 +140,10 @@ BFA <- function(file){
   }
   save(list_containers, file = "BFA.RData")
   containers_count <- length(list_containers)
-  return(containers_count)
+  return(c(containers_count, count))
 }
   
-file <- scan(file = "table.txt")
+file <- scan(file = "table.txt", nlines = 1)
 min_count_containers(file)
 NFA(file)
 FFA(file)
