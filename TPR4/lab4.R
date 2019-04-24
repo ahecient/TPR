e69589976043474384f5aed69bf97cb62937f5d1
ahecient <48700087+ahecient@users.noreply.github.com>
2019-04-25 00:44:50 +0300

    Compl +

--- TPR4/lab4.R
+++ TPR4/lab4.R
@@ -28,31 +28,41 @@ Borde <- function(data){
   return(t(lst))
 }
 
+Compl <- function(data){
+  voters <- colnames(data)
+  voters <- as.integer(voters)
+  lst_complend <- list()
+  candidate <- sort(unique(data[,1]))
+  for(i in 1:length(candidate)){
+    nul <- 0
+    lst_complend[[candidate[i]]] <- nul
+  }
+  
+  for(i in 1:length(candidate)){
+    for(j in 1:length(candidate)){
+      first_candidate <- 0
+      second_candidate <- 0
+      if(i != j & i < j){
+        for(k in 1:ncol(data)){
+          if(which(data[,k]==candidate[i]) < which(data[,k]==candidate[j])) first_candidate <- first_candidate+voters[k]
+          else second_candidate <- second_candidate+voters[k]
+        }
+        if(first_candidate > second_candidate) lst_complend[[candidate[i]]] <- lst_complend[[candidate[i]]] + 1 
+        else lst_complend[[candidate[j]]] <- lst_complend[[candidate[j]]] - 1
+        #print(paste(string[i], string[j], sep = ":"))
+        #print(c(y,x))
+      }
+    }
+  }
+  
+  lst_complend <- lst_complend[order(-unlist(lst_complend))]
+  lst_complend <- as.matrix(lst_complend)
+  
+  return(t(lst_complend))
+}
 
 data <- read.table("table.txt", header = TRUE, sep = " ", check.names = FALSE, stringsAsFactors = FALSE)
 Borde(data)
+Compl(data)
 
-v <- colnames(data)
-v <- as.integer(v)
-lst <- list("a" = 0, "b" = 0, "c" = 0, "d" = 0)
-for(j in 1:ncol(data)){                               #< - win
-  
-  if(which(data[,j]=="a")<which(data[,j]=="b")) lst[["a"]] <- lst[["a"]]+1
-  else lst[["b"]] <- lst[["b"]]-1
-  
-  if(which(data[,j]=="a")<which(data[,j]=="c")) lst[["a"]] <- lst[["a"]]+1
-  else lst[["c"]] <- lst[["c"]]-1
-  
-  if(which(data[,j]=="a")<which(data[,j]=="d")) lst[["a"]] <- lst[["a"]]+1
-  else lst[["d"]] <- lst[["d"]]-1
-  
-  if(which(data[,j]=="b")<which(data[,j]=="d")) lst[["b"]] <- lst[["b"]]+1
-  else lst[["d"]] <- lst[["d"]]-1
-  
-  if(which(data[,j]=="b")<which(data[,j]=="c")) lst[["b"]] <- lst[["b"]]+1
-  else lst[["c"]] <- lst[["c"]]-1
-  
-  if(which(data[,j]=="c")<which(data[,j]=="c")) lst[["c"]] <- lst[["c"]]+1
-  else lst[["d"]] <- lst[["d"]]-1
-}
 
