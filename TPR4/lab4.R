5d5ba569f27b180ca76bc54f517edbc2858d7a5f
ahecient <48700087+ahecient@users.noreply.github.com>
2019-04-24 22:27:29 +0300

    borde +

--- TPR4/lab4.R
+++ TPR4/lab4.R
@@ -1,38 +1,33 @@
-
-data <- read.table("table.txt", header = TRUE, sep = " ", check.names = FALSE, stringsAsFactors = FALSE)
-
-
-v <- colnames(data)
-v <- as.integer(v)
-
-data$S <- ""
-
-weigth <- 0
-for(i in nrow(data):1){
-  data$S[i] <- weigth
-  weigth <- weigth + 1
+Borde <- function(data){
+  v <- colnames(data)
+  v <- as.integer(v)
+  
+  data$S <- ""
+  
+  weigth <- 0
+  
+  for(i in nrow(data):1){
+    data$S[i] <- weigth
+    weigth <- weigth + 1
+  }
+  
+  data$S <- as.integer(data$S)
+  lst <- list()
+  for(j in 1:(ncol(data)-1)){
+    lst[["a"]] <- append(lst[["a"]], (data$S[which(data[,j]=="a")]))
+    lst[["b"]] <- append(lst[["b"]], (data$S[which(data[,j]=="b")]))
+    lst[["c"]] <- append(lst[["c"]], (data$S[which(data[,j]=="c")]))
+    lst[["d"]] <- append(lst[["d"]], (data$S[which(data[,j]=="d")]))
+  }
+  
+  lst <- lapply(lst, "*", v)
+  lst <- lapply(lst, function(x) sum(x))
+  lst <- lst[order(-unlist(lst))]
+  
+  return(names(lst))
 }
-data$S <- as.integer(data$S)
-na <- NULL
-nb <- NULL
-nc <- NULL
-nd <- NULL
-for(j in 1:(ncol(data)-1)){
-    na <- append(na, (data$S[which(data[,j]=="a")]))
-    nb <- append(nb, (data$S[which(data[,j]=="b")]))
-    nc <- append(nc, (data$S[which(data[,j]=="c")]))
-    nd <- append(nd, (data$S[which(data[,j]=="d")]))
-}
-na <- na*v
-na <- sum(na)
-nb <- nb*v
-nb <- sum(na)
-nc <- nc*v
-nc <- sum(nc)
-nd <- nd*v
-nd <- sum(nd)
 
-summ <- function(x){
-  x <- x*v
-  x <- sum(x)
-}
+
+table <- read.table("table.txt", header = TRUE, sep = " ", check.names = FALSE, stringsAsFactors = FALSE)
+Borde(table)
+
