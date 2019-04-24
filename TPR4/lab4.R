934a31ce99ea6d24a78c2f158a0ffbcb5a9ca4b5
ahecient <48700087+ahecient@users.noreply.github.com>
2019-04-24 23:18:08 +0300

    compl +\--

--- TPR4/lab4.R
+++ TPR4/lab4.R
@@ -23,11 +23,36 @@ Borde <- function(data){
   lst <- lapply(lst, "*", v)
   lst <- lapply(lst, function(x) sum(x))
   lst <- lst[order(-unlist(lst))]
+  lst <- as.matrix(lst)
   
-  return(names(lst))
+  return(t(lst))
 }
 
 
-table <- read.table("table.txt", header = TRUE, sep = " ", check.names = FALSE, stringsAsFactors = FALSE)
-Borde(table)
+data <- read.table("table.txt", header = TRUE, sep = " ", check.names = FALSE, stringsAsFactors = FALSE)
+Borde(data)
+
+v <- colnames(data)
+v <- as.integer(v)
+lst <- list("a" = 0, "b" = 0, "c" = 0, "d" = 0)
+for(j in 1:ncol(data)){                               #< - win
+  
+  if(which(data[,j]=="a")<which(data[,j]=="b")) lst[["a"]] <- lst[["a"]]+1
+  else lst[["b"]] <- lst[["b"]]-1
+  
+  if(which(data[,j]=="a")<which(data[,j]=="c")) lst[["a"]] <- lst[["a"]]+1
+  else lst[["c"]] <- lst[["c"]]-1
+  
+  if(which(data[,j]=="a")<which(data[,j]=="d")) lst[["a"]] <- lst[["a"]]+1
+  else lst[["d"]] <- lst[["d"]]-1
+  
+  if(which(data[,j]=="b")<which(data[,j]=="d")) lst[["b"]] <- lst[["b"]]+1
+  else lst[["d"]] <- lst[["d"]]-1
+  
+  if(which(data[,j]=="b")<which(data[,j]=="c")) lst[["b"]] <- lst[["b"]]+1
+  else lst[["c"]] <- lst[["c"]]-1
+  
+  if(which(data[,j]=="c")<which(data[,j]=="c")) lst[["c"]] <- lst[["c"]]+1
+  else lst[["d"]] <- lst[["d"]]-1
+}
 
