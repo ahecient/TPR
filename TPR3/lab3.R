d02db8b625ca7235861e17a1938e843bc3854ae0
ahecient <48700087+ahecient@users.noreply.github.com>
2019-04-25 19:06:51 +0300

    adding data

---
+++ TPR3/lab3.R
@@ -0,0 +1,2 @@
+ClothesTable <- read.table("Clothes.txt", stringsAsFactors = FALSE, sep = "\t", dec = ".", header = TRUE, check.names = FALSE)
+SeasonsTable <- read.table("Seasons.txt", stringsAsFactors = FALSE, sep = "\t", header = TRUE, check.names = FALSE)
